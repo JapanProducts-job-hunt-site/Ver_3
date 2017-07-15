3f33957cc5a82ea864c807225640c61a32ad4fea
YukiKuroshima <yukikuro1993@gmail.com>
2017-07-14 17:07:40 -0700

    Gitignore

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
 	'secret': 'ilovescotchyscotch',
-	'database': 'mongodb://1:1@ds157712.mlab.com:57712/react-jwt-simple'
+	'database': 'mongodb://1:1@ds157702.mlab.com:57702/ver3'
 
 };
