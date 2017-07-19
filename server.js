cc61e598b41f8cc6f58e681dba5fed4628671289
YukiKuroshima <yukikuro1993@gmail.com>
2017-07-19 11:22:55 -0700

    Done test for GET /

--- server.js
+++ server.js
@@ -14,15 +14,26 @@ const User   = require('./api/models/user'); // get our mongoose model
 // configuration ===================================================
 // =================================================================
 const port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
+
+//don't show the log when it is test
+// Connect to test db when it is test
+if(process.env.NODE_ENV !== 'test') {
+	//use morgan to log at command line
+	app.use(morgan('dev'));
+	// Connect to read database
+	mongoose.connect(config.database);
+}
+else if(process.env.NODE_ENV === 'test') {
+	// Connect to test database
+	mongoose.connect(config.test_db); 
+}
+
 app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// use morgan to log requests to the console
-app.use(morgan('dev'));
 
 // Tell the server to look at these directories to look for static files
 app.use(express.static('./static/'));
@@ -54,9 +65,9 @@ app.get('/setup', function(req, res) {
 });
 
 // basic route (http://localhost:8080)
-// app.get('/', function(req, res) {
-// 	res.send('Hello! The API is at http://localhost:' + port + '/api');
-// });
+app.get('/', function(req, res) {
+	res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
 
 // ---------------------------------------------------------
 // get an instance of the router for api routes
--- test/server.js
+++ test/server.js
@@ -2,10 +2,39 @@
 process.env.NODE_ENV = 'test';
 
 let mongoose = require("mongoose");
-let Book = require('../app/models/book');
+let User = require('../api/models/user');
 
 //Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+
+const port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+
+chai.use(chaiHttp);
+
+describe('User', () => {
+	beforeEach((done) => {
+		User.remove({}, (err) => {
+			done();
+		});
+	});
+
+	// Test /
+	describe('GET /', () => {
+		it('it should say hello', (done) => {
+			// chai.request(server)
+			chai.request('http://localhost:' + port)
+				.get('/')    
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.should.have.property('text');
+					res.text.should.be.a('string');
+					res.text.should.include('Hello!');
+					done();
+				});
+		});
+	});
+});
