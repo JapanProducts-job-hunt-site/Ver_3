8991de80390b9c813abb260ecbbb0d51b05a1f61
YukiKuroshima <yukikuro1993@gmail.com>
2017-10-09 12:32:46 -0700

    :tada:

---
+++ client/static/js/user/profile.js
@@ -0,0 +1,58 @@
+/*
+ * This js file is for profile function
+ */
+$(document).ready(function() {
+  console.log("Profile page ready. Start ");
+});
+
+$( "form#signin" ).submit(function( event ) {
+  event.preventDefault();
+
+  var email = $("form#signin input.email").val();
+  var password = $("form#signin input.password").val();
+
+  var method = "POST";
+  var url = "api/authenticate";
+  var contentType = "application/x-www-form-urlencoded";
+  // To enable object formatter for query string
+  var processData = true;
+  // This will be transformed into query string
+  var data = {
+    email: email,
+    password: password,
+  };
+  var dataType = "json";
+
+  // Only for testing
+  console.log("Email: " + email);
+  console.log("Password: " + password);
+
+  $.ajax({
+    method: method,
+    url: url,
+    contentType: contentType,
+    processData: processData,
+    data: data,
+    dataType: dataType,
+  })
+  /*
+   * Excecuted when success
+   */
+  .done(function (data) {
+    console.log(data.token);
+    // Store the token in localStorage
+    localStorage.setItem('token', data.token);
+  })
+  /*
+   * Excecuted when unsuccessful
+   */
+  .fail(function (jqXHR) {
+    // Invoke error pop-up
+    $('.signin-container .error-bg').fadeIn(500)
+    $('.error-bg .retry').click(function() {
+      $('.signin-container .error-bg').fadeOut(500)
+    })
+
+    console.log(jqXHR.responseJSON);
+  })
+});
